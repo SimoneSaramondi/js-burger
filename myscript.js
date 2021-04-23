272d89fc49f7ecb6d64a296e7644b90620b7eb9c
SimoneSaramondi <simo98sara@gmail.com>
2021-04-23 17:39:33 +0200

    project review

--- myscript.js
+++ myscript.js
@@ -3,49 +3,57 @@ var toCalcolate = document.getElementById("button");
 toCalcolate.addEventListener("click", function(){
 
     // REUPERA IL NOME DELL'HAMBURGER
-var hamburger = document.getElementById("name");
-var hamburgerName = hamburger.value;
+    var hamburger = document.getElementById("name");
+    var hamburgerName = hamburger.value;
 
 
-//SE NON E' PRESENTE IL NOME DEL PANINO NON NE CALCOLA IL PREZZO 
-if(hamburgerName === ""){
-    alert("Inserisci il nome dell'hamburger");
-} else{
-// console.log(hamburgerName);
+    //SE NON E' PRESENTE IL NOME DEL PANINO NON NE CALCOLA IL PREZZO 
+    if(hamburgerName === ""){
+        alert("Inserisci il nome dell'hamburger");
+    } else{
+    // console.log(hamburgerName);
 
     // INSERISCO TUTTI GLI INGREDIENTI IN UNO PSEUDO-ARRAY
     // Solo qualli con classe ingredient e type = "checkbox" 
-var ingredientsList = document.querySelectorAll(".ingredient [type='checkbox']");
+    var ingredientsList = document.querySelectorAll(".ingredient [type='checkbox']");
 
-var somma = 50;
+    var somma = 50;
 
     // IL CICLO FUNZIONE FINO A QUANDO NON HA FATTO PASSARE TUTTI GLI INGREDIENTI
-for(var i = 0; i < ingredientsList.length; i++){
-    var ingredient = ingredientsList[i];
-    // Se l'elemento su cui stiamo passando è segnato prendo il prezzo
-    // di questo tramite value e lo sommo al generale
-    if(ingredient.checked){
-        somma += parseInt(ingredient.value)
+    for(var i = 0; i < ingredientsList.length; i++){
+        var ingredient = ingredientsList[i];
+        // Se l'elemento su cui stiamo passando è segnato prendo il prezzo
+        // di questo tramite value e lo sommo al generale
+        if(ingredient.checked){
+            somma += parseInt(ingredient.value)
+        }
     }
-}
+    console.log("prima: " + somma);
 
-function price(cash, discount){
-    return cash = cash - (cash * discount);
-}
+    function price(cash, discount){
+        return cash = cash - (cash * discount);
+    }
 
-var couponOne = "12345ABCDEF";
-var couponTwo = "54321ABCDEF";
-var couponThree = "12345FEDCBA";
-var discountActive = 0.05 // Sconto del 5% sul prezzo totale del panino
+    var couponOne = "12345ABCDEF";
+    var couponTwo = "54321ABCDEF";
+    var couponThree = "12345FEDCBA";
+    var discountActive = 0.05 // Sconto del 5% sul prezzo totale del panino
 
-var coupon = document.getElementById("coupon");
-var couponTrue = coupon.value;
+    var coupon = document.getElementById("coupon");
+    var couponTrue = coupon.value;
 
-if(couponTrue === couponOne || couponTrue === couponTwo || couponTrue === couponThree ){
     var priceBurger = price(somma, discountActive);
-}
+    console.log("seconda: " + priceBurger);
 
-document.getElementById("price").innerText = priceBurger;
+    if(couponTrue === couponOne || couponTrue === couponTwo || couponTrue === couponThree ){
+        document.getElementById("price").innerHTML = priceBurger;
+    }else {
+        document.getElementById("price").innerHTML = somma;
+        console.log("Terza" + somma);
+    }
+
+    
+    
 
 }
 })
